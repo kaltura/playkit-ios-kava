f9e03aacaece6592415361ee0498966425bceb39
Nilit Danan <Nilit.danan@kaltura.com>
2020-03-29 14:47:14 +0300

    FEC-9930 Kava PlayRequest event is not sent upon change media. (#38)
    * FEC-9809 Added validation to send the PlayRequest event only once per session.
    
    * FEC-9930 Need to stop the view timer and reset the sessionStartTime upon PlayerEvent Stop as well.
    
    * Fixed Indentation.

--- Sources/KavaPlugin+AnalyticsPluginProtocol.swift
+++ Sources/KavaPlugin+AnalyticsPluginProtocol.swift
@@ -18,12 +18,12 @@ extension KavaPlugin: AnalyticsPluginProtocol {
     /************************************************************/
     
     public var isFirstPlayRequest: Bool {
-           get {
-               return self.kavaData.isFirstPlayRequest
-           }
-           set {
-               self.kavaData.isFirstPlayRequest = newValue
-           }
+        get {
+            return self.kavaData.isFirstPlayRequest
+        }
+        set {
+            self.kavaData.isFirstPlayRequest = newValue
+        }
     }
     
     public var isFirstPlay: Bool {
@@ -44,6 +44,7 @@ extension KavaPlugin: AnalyticsPluginProtocol {
             PlayerEvent.playing,
             PlayerEvent.seeking,
             PlayerEvent.sourceSelected,
+            PlayerEvent.stopped,
             PlayerEvent.ended,
             PlayerEvent.replay,
             PlayerEvent.textTrackChanged,
@@ -72,6 +73,8 @@ extension KavaPlugin: AnalyticsPluginProtocol {
                 self.handleSeeking(targetSeekPosition: event.targetSeekPosition)
             case is PlayerEvent.SourceSelected:
                 self.handleSourceSelected(mediaSource: event.mediaSource)
+            case is PlayerEvent.Stopped:
+                self.handleStopped()
             case is PlayerEvent.Ended:
                 self.handleEnded()
             case is PlayerEvent.Replay:
@@ -186,6 +189,11 @@ extension KavaPlugin: AnalyticsPluginProtocol {
             self.kavaData.selectedSource = source
             self.updateDeliveryType(mediaFormat: source.mediaFormat)
         }
+    }
+    
+    private func handleStopped() {
+        PKLog.debug("stopped event")
+        self.stopViewTimer()
     }
     
     private func handleEnded() {
